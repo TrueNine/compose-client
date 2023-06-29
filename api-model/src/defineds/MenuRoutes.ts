e03c709f9211b546c223b65b8bba8cc35ea19df0
TrueNine <truenine304520@gmail.com>
2023-06-29 18:54:29 +0800

    fix 改变菜单递归的位置

--- api-model/src/defineds/MenuRoutes.ts
+++ api-model/src/defineds/MenuRoutes.ts
@@ -15,18 +15,18 @@ export interface RouteOption {
 /**
  * ## 以 url 查找匹配的菜单节点，以 uri 进行匹配
  * @param paths 路径
+ * @param subs 菜单的所有节点
  * @param deep 递归层级
  * @param root 根元素
- * @param subs 子节点
  * @returns 查找到的菜单节点
  */
-export function findMenu(paths: string[], deep = 0, root: RouteOption | null = null, subs: RouteOption[] = []): Nullable<RouteOption> {
+export function findMenu(paths: string[], subs: RouteOption[] = [], deep = 0, root: RouteOption | null = null): Nullable<RouteOption> {
   if (paths.length === deep) return root
   let result: Nullable<RouteOption> = null
   for (let i = 0; i < subs.length; i++) {
     const it = subs[i]
     if (it.uri?.replace('/', '') === paths[deep]) {
-      result = findMenu(paths, deep + 1, it, it.sub ?? [])
+      result = findMenu(paths, it.sub ?? [], deep + 1, it)
       break
     }
   }
