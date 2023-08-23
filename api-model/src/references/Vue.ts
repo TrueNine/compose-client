0b73cdd2c4df1258cbd7b558fa429b59e0dbb2f4
TrueNine <truenine304520@gmail.com>
2023-08-23 17:15:13 +0800

    fix 改善 vue 工具类的使用

--- api-model/src/references/Vue.ts
+++ api-model/src/references/Vue.ts
@@ -8,25 +8,21 @@ interface _InstallInvoke {
 }
 
 /**
- * ## 安装一个组件
- * @param component 组件
- * @returns 封装后的组件
- */
-function withInstall<T>(component: T): T & Plugin {
-  const _p = component as T & Plugin
-  _p.install = app => {
-    const {name, __name = undefined} = _p as unknown as T & Plugin & _InstallInvoke
-    if (name) app.component(name, _p)
-    else app.component(__name ?? 'NameUndefined', _p)
-  }
-  return _p
-}
-
-/**
  * ## 针对 vue 封装的一些工具函数
  */
-export function withComponent() {
-  return {
-    withInstall
+export class withComponent {
+  /**
+   * ## 安装一个组件
+   * @param component 组件
+   * @returns 封装后的组件
+   */
+  static withInstall<T>(component: T): T & Plugin & _InstallInvoke {
+    const _p = component as T & Plugin
+    _p.install = app => {
+      const {name, __name = undefined} = _p as unknown as T & Plugin & _InstallInvoke
+      if (name) app.component(name, _p)
+      else app.component(__name ?? 'NameUndefined', _p)
+    }
+    return _p
   }
 }
