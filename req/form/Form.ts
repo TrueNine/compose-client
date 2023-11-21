f4f6e83a0734c27f1bb6bd929ebb66b76bbf53b1
TrueNine <truenine304520@gmail.com>
2023-11-21 21:09:24 +0800

    fix 修复表单的空校验

--- req/form/Form.ts
+++ req/form/Form.ts
@@ -4,7 +4,7 @@ export function form(formContent: object): FormData {
   if (!formContent) return new FormData()
   const f = new FormData()
   Object.entries(formContent).forEach(([k, v]) => {
-    if (v != null && v != STR_EMPTY) {
+    if (v != null && v !== STR_EMPTY) {
       if (Array.isArray(v)) f.append(k, v.map(String).join(','))
       if (v instanceof Blob) f.append(k, v)
       else f.append(k, v.toString())
