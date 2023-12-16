d0d5e47f5bcfe65bf7aeeb086e183ba757be60ef
TrueNine <truenine304520@gmail.com>
2023-12-16 12:28:12 +0800

    upd 更新 eslint

--- compose-types/orm/Utils.ts
+++ compose-types/orm/Utils.ts
@@ -21,6 +21,8 @@ export type Id = string
  * 数据库的外键
  */
 export type ReferenceId = Id
+export type RefId = ReferenceId
+
 /**
  * 大数，用于相加或随机增加的大数值
  */
@@ -41,3 +43,5 @@ export type TypeInt = Int
  * 字符串类型序列号
  */
 export type TypeSerial = SerialCode
+export type TypeString = TypeSerial
+export type TypeStr = TypeString
