0de585415cf314a3d1d7aa27c369c7d42a1e2d9f
赵日天 <truenine304520@gmail.com>
2024-06-16 09:33:38 +0800

    fix 近似修复 query 参数数组空逗号问题

--- req/src/form/EncodeComponent.ts
+++ req/src/form/EncodeComponent.ts
@@ -1,5 +1,5 @@
-import type {BasicType, nullpt} from '@compose/api-types'
-import {isNonEmpty, isNonEmptyString} from '@compose/api-model'
+import type {BasicType, nilpt} from '@compose/api-types'
+import {isNonNil, isNonNilString} from '@compose/api-model'
 
 export class SearchParam {
   private _root: Map<string, BasicType> = new Map()
@@ -19,33 +19,33 @@ export class SearchParam {
  * # 将多个对象转换为 URLSearchParams 格式的字符串
  * @param cards 需转换对象
  */
-export function encodeQueryParam(...cards: nullpt<object>[]): string {
+export function encodeQueryParam(...cards: nilpt<object>[]): string {
   const params = new SearchParam()
   if (!cards) return ''
-  cards.filter(isNonEmpty).forEach(c => {
+  cards.filter(isNonNil).forEach(c => {
     Object.entries(c as Record<string, unknown>)
-      .filter(([, v]) => isNonEmpty(v))
-      .map(([k, v]) => [k, isNonEmptyString(v as string) ? encodeURIComponent(v as string) : v])
+      .filter(([, v]) => isNonNil(v))
+      .map(([k, v]) => [k, isNonNilString(v as string) ? encodeURIComponent(v as string) : v])
       .map(([k, v]) => [k, Array.isArray(v) ? v.join(',') : v])
       .forEach(([k, v]) => params.append(encodeURIComponent(k as string), v as string))
   })
   return params.toString() && `?${params.toString()}`
 }
 
-export function queryParam(...cards: nullpt<object>[]): string {
+export function queryParam(...cards: nilpt<object>[]): string {
   const params = new SearchParam()
   if (!cards) return ''
-  cards.filter(isNonEmpty).forEach(c => {
+  cards.filter(isNonNil).forEach(c => {
     Object.entries(c as Record<string, unknown>)
-      .filter(([, v]) => isNonEmpty(v))
+      .filter(([, v]) => isNonNil(v))
       .map(([k, v]) => [k, v])
-      .map(([k, v]) => [k, Array.isArray(v) ? v.join(',') : v])
+      .map(([k, v]) => [k, Array.isArray(v) ? v.filter(isNonNil).join(',') : v])
       .forEach(([k, v]) => params.append(k as string, v as string))
   })
   return params.toString() && `?${params.toString()}`
 }
 
-export function queryHash(...cards: nullpt<object>[]): string {
+export function queryHash(...cards: nilpt<object>[]): string {
   const qp = queryParam(...cards)
   return qp ? `#${qp.slice(1)}` : ''
 }
