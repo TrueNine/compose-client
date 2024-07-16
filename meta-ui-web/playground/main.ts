a8315ed9d4731dbab4087b01e41f120459e58013
赵日天 <truenine304520@gmail.com>
2024-07-17 04:31:05 +0800

    test 对 权限组件进行测试

--- meta-ui-web/playground/main.ts
+++ meta-ui-web/playground/main.ts
@@ -10,12 +10,14 @@ import {createVuetifyMount, quasarInstall} from '@/common'
 import {naiveStyleAdaptor} from '@/index'
 
 import 'uno.css'
+import {Pinia} from './store'
 
 export const a = createVuetifyMount(r => Object.assign({}, r))
 async function setupApp() {
   naiveStyleAdaptor()
   const createdApp = createApp(App)
   quasarInstall(createdApp)
+  createdApp.use(Pinia)
   createdApp.use(a)
   createdApp.use(Router)
   createdApp.mount('#ROOT')
