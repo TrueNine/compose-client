df74770274c45fc04b2338972c18ab05e852f44a
赵日天 <truenine304520@gmail.com>
2024-07-17 04:31:39 +0800

    feat 增加权限控制 use 函数

--- meta-ui-web/src/common/Yan100Common.ts
+++ meta-ui-web/src/common/Yan100Common.ts
@@ -1,1 +1,29 @@
+import type {InjectionKey} from '@vue/runtime-core'
+import type {Ref} from 'vue'
+import type {Maybe} from '@compose/api-types'
 export {}
+
+const providePrefix = '__MetaUIProvideSymbolKey__'
+
+function getProvideSymbolKey<T = unknown>(key: string): InjectionKey<T> {
+  return Symbol(`${providePrefix}${key}`)
+}
+
+export interface PreAuthorizeInjection {
+  hasAnyPermissions: (permissions: Maybe<string>) => boolean
+  requirePermissions: (permissions: Maybe<string>) => void
+  hasAnyRoles: (roles: Maybe<string>) => boolean
+  requireRoles: (roles: Maybe<string>) => void
+  isAuthed: () => boolean
+  isAnonymous: () => boolean
+  permissions: Ref<string[]>
+  roles: Ref<string[]>
+  authed: Ref<boolean>
+  anonymous: Ref<boolean>
+}
+
+export const PreAuthorizeInjectionSymbol: InjectionKey<PreAuthorizeInjection> = getProvideSymbolKey('PreAuthorizeInjection')
+
+export function usePreAuthorize() {
+  return inject(PreAuthorizeInjectionSymbol)!
+}
