c01de63f3961e6b540dae8a0cdbb700a30ddd665
赵日天 <truenine304520@gmail.com>
2024-07-17 04:30:40 +0800

    test 对 权限组件进行测试

---
+++ meta-ui-web/playground/pages/pre-authorize/AuthPlaygroundPage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import YPreAuthorize from '@/pre-authorize'
+import {usePlaygroundStore} from '../../store'
+
+const store = usePlaygroundStore()
+
+const acl = () => {
+  return store.anon
+}
+setTimeout(() => {
+  store.anon = true
+}, 3000)
+</script>
+
+<template>
+  <YConfigPreAuthorize :anonymous-provider="acl">
+    <YPreAuthorize>
+      <template #default="e">
+        {{ e }}
+      </template>
+    </YPreAuthorize>
+  </YConfigPreAuthorize>
+</template>
