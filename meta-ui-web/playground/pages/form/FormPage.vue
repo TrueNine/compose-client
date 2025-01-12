56e0fb0109fe635a697f5d6b5c986a73ee168769
赵日天 <truenine304520@gmail.com>
2025-01-12 18:15:27 +0800

    refactor(playground): 重构表单页面并添加测试代码
    - 引入 YForm 组件并替换原有模板结构
    - 添加 console.log 输出用于调试
    - 设置定时器修改 mv 值以测试响应式更新
    - 更新路由配置，明确指定表单页面路由

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {object} from 'yup'
 import type {dynamic} from '@compose/api-types'
+import YForm from '@/form/YForm.vue'
 
 const schema = [object().shape({}), object().shape({}), object().shape({})]
 const mv = ref()
+console.log(mv)
 const updMv = () => {
   mv.value = {
     x: 1,
@@ -16,22 +18,31 @@ const submit = (y: dynamic) => {
   console.log(y)
 }
 const step = ref(2)
+setTimeout(() => {
+  console.log(mv)
+  mv.value.x = 1
+}, 2000)
 </script>
 
 <template>
   <div>
     <h1>{{ mv }}</h1>
     <VBtn @click="updMv">updMV</VBtn>
+
     <YForm v-model:step="step" v-model="mv" :schema="schema" @next="submit" @submit="submit">
       <YField name="x">
         <template #input="p">
           <h1>{{ p }}</h1>
-          <YTestDefaultValid v-bind="p" />
+          <VTextField v-bind="p" />
         </template>
       </YField>
       <YField name="y">1</YField>
       <YField name="z">2</YField>
-      <VBtn type="submit">submit</VBtn>
+      <div>
+        <div>
+          <VBtn type="submit">submit</VBtn>
+        </div>
+      </div>
     </YForm>
   </div>
 </template>
--- meta-ui-web/playground/router/index.ts
+++ meta-ui-web/playground/router/index.ts
@@ -1,10 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import {resolveRouters} from '@compose/api-model'
 
-const a = resolveRouters()
 const Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...a]
+  routes: [{
+    path: '/form',
+    component: () => import('../pages/form/FormPage.vue')
+  }]
 })
 
 export {Router}
