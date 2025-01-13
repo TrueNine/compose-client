ed514bcd3f636baf158aed56499e382e7bd4aab3
赵日天 <truenine304520@gmail.com>
2025-01-13 12:14:18 +0800

    refactor(form): 简化 FormPage 组件
    - 移除了不必要的代码和逻辑
    - 优化了表单结构和布局- 添加了 submit 函数的简单实现

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -3,46 +3,36 @@ import {object} from 'yup'
 import type {dynamic} from '@compose/api-types'
 import YForm from '@/form/YForm.vue'
 
-const schema = [object().shape({}), object().shape({}), object().shape({})]
+const schema = object().shape({})
 const mv = ref()
-console.log(mv)
-const updMv = () => {
-  mv.value = {
-    x: 1,
-    y: 2,
-    z: 3
-  }
+
+const submit = (f: dynamic) => {
+  console.log('page submit')
+  console.log(f)
 }
-const submit = (y: dynamic) => {
-  console.log('submit')
-  console.log(y)
-}
-const step = ref(2)
-setTimeout(() => {
-  console.log(mv)
-  mv.value.x = 1
-}, 2000)
 </script>
 
 <template>
-  <div>
-    <h1>{{ mv }}</h1>
-    <VBtn @click="updMv">updMV</VBtn>
-
-    <YForm v-model:step="step" v-model="mv" :schema="schema" @next="submit" @submit="submit">
-      <YField name="x">
-        <template #input="p">
-          <h1>{{ p }}</h1>
-          <VTextField v-bind="p" />
-        </template>
-      </YField>
-      <YField name="y">1</YField>
-      <YField name="z">2</YField>
+  <YForm v-model="mv" :schema="schema" @next="submit" @submit="submit">
+    <YField name="x">
+      <template #input="p">
+        <VTextField v-bind="p" />
+      </template>
+    </YField>
+    <YField name="y">
+      <template #input="p">
+        <VTextField v-bind="p" />
+      </template>
+    </YField>
+    <YField name="z">
+      <template #input="p">
+        <VTextField v-bind="p" />
+      </template>
+    </YField>
+    <div>
       <div>
-        <div>
-          <VBtn type="submit">submit</VBtn>
-        </div>
+        <VBtn type="submit">submit</VBtn>
       </div>
-    </YForm>
-  </div>
+    </div>
+  </YForm>
 </template>
