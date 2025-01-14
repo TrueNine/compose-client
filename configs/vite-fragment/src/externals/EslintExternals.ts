52d63e4d18ea56fae49f532c2f2d10b5a1736155
赵日天 <truenine304520@gmail.com>
2025-01-14 10:45:36 +0800

    build(vite-fragment): 升级版本并添加 ESLint 插件支持
    - 将 configs/vite-fragment 版本从0.0.15 升级到 0.0.16
    - 在 EslintExternals.ts 中添加对 ESLint插件的正则匹配规则

--- configs/vite-fragment/src/externals/EslintExternals.ts
+++ configs/vite-fragment/src/externals/EslintExternals.ts
@@ -3,4 +3,5 @@ export const EslintExternals = [
   /^(@rushstack|@rushstack\/)/,
   /^(eslint-define-config$|eslint-define-config\/)/,
   /^(eslint$|eslint\/|@eslint|@eslint\/)/,
+  /^eslint-plugin-/,
 ]
