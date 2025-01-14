89c57f92468ab48df862df05c2f05c8708be982a
赵日天 <truenine304520@gmail.com>
2025-01-14 08:53:32 +0800

    feat(lodash-es): 添加 cloneDeepWith 函数
    - 从 lodash-es 库中引入 cloneDeepWith 函数
    - 在 extensions/src/lodash-es/index.ts 中导出 cloneDeepWith
    - 更新 extensions/package.json 版本号至 0.0.30

--- extensions/src/lodash-es/index.ts
+++ extensions/src/lodash-es/index.ts
@@ -1,8 +1,10 @@
 export {}
 import _cloneDeep from 'lodash-es/cloneDeep'
+import _cloneDeepWith from 'lodash-es/cloneDeepWith'
 import _merge from 'lodash-es/merge'
 import _isEqual from 'lodash-es/isEqual'
 
 export const cloneDeep = _cloneDeep
 export const merge = _merge
 export const isEqual = _isEqual
+export const cloneDeepWith = _cloneDeepWith
