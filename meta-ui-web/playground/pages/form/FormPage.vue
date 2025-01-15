03ec2bd5d531eb74eaf4ceb28990279f6eaa5407
赵日天 <truenine304520@gmail.com>
2025-01-15 20:59:34 +0800

    refactor(form): 重构表单页面并添加文件输入功能
    - 简化了表单结构，移除了不必要的字段
    - 添加了两个文件输入字段
    - 更新了提交按钮样式
    - 在脚本中添加了对文件列表只读性的测试

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -4,50 +4,27 @@ import type {dynamic} from '@compose/api-types'
 import YForm from '@/form/YForm.vue'
 
 const schema = object().shape({})
-const mv = ref()
-
+const mv = ref<{files: File[]}>({files: []})
+console.log('page is readonly', isReadonly(mv.value.files))
 const submit = (f: dynamic) => {
-  console.log('page submit')
-  console.log(f)
+  console.log('page submit', f)
 }
-
+setTimeout(() => {
+  console.log('mv is readonly', isReadonly(mv.value.files))
+  mv.value = {
+    files: [new File(['1'], '1'), new File(['1'], '2')]
+  }
+}, 1000)
 </script>
 
 <template>
-  <YForm class="w-50vw p-4" v-model="mv" :schema="schema" @next="submit" @submit="submit">
-    <YField name="defaultSlot.component" label="y field props">
-      <VTextField />
+  <YForm class="w-50vw p-4" :modelValue="mv" :schema="schema" @next="submit" @submit="submit">
+    <YField name="files[0]">
+      <VFileInput />
     </YField>
-
-    <YField name="defaultSlot.empty"></YField>
-    <YField name="defaultSlot.text"> text</YField>
-    <YField name="defaultSlot.variableText">
-      {{ 'text' }}
+    <YField name="files[1]">
+      <VFileInput />
     </YField>
-
-    <YField name="defaultSlot.div" effectModels="modelValue">
-      <div class="bg-red">DIV TEXT</div>
-    </YField>
-    <YField name="defaultSlot.input">
-      <input />
-    </YField>
-
-    <YField name="a.b">
-      <VTextField />
-    </YField>
-    <YField name="x">
-      <VTextField />
-    </YField>
-    <YField name="y">
-      <VTextField />
-    </YField>
-    <YField name="z">
-      <VTextField  />
-    </YField>
-    <div>
-      <div>
-        <VBtn type="submit">submit</VBtn>
-      </div>
-    </div>
+    <VBtn type="submit" block color="primary">SUBMIT</VBtn>
   </YForm>
 </template>
