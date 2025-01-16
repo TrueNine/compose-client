d2007f5ee8f0c111d1f1683b611bd83b2c3f0ee8
赵日天 <truenine304520@gmail.com>
2025-01-16 15:30:39 +0800

    feat(form): 添加文件上传校验规则
    - 为文件上传字段添加了 YForm schema 校验规则
    - 限制文件类型为 PDF 或 PNG
    - 限制最多上传一个文件
    - 移除了不必要的 console.log 语句和测试代码

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
-import {object} from 'yup'
+import {array, object, string} from 'yup'
 import type {dynamic} from '@compose/api-types'
-import YForm from '@/form/YForm.vue'
 
-const schema = object().shape({})
-const mv = ref<{files: File[]}>({files: []})
-console.log('page is readonly', isReadonly(mv.value.files))
+const schema = object({
+  files: array()
+    .of(
+      object({
+        type: string().required().oneOf(['application/pdf', 'application/png'])
+      })
+    )
+    .max(1)
+})
 const submit = (f: dynamic) => {
   console.log('page submit', f)
 }
-setTimeout(() => {
-  console.log('mv is readonly', isReadonly(mv.value.files))
-  mv.value = {
-    files: [new File(['1'], '1'), new File(['1'], '2')]
-  }
-}, 1000)
 </script>
 
 <template>
-  <YForm class="w-50vw p-4" :modelValue="mv" :schema="schema" @next="submit" @submit="submit">
+  <YForm class="w-50vw p-4" :schema="schema" @submit="submit">
+    <YFieldMessage name="files" />
     <YField name="files[0]">
       <VFileInput />
     </YField>
