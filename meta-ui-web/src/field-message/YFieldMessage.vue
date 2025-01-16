a918fbff1b5ca5e09426e7e27f3947e9e23dbbaa
赵日天 <truenine304520@gmail.com>
2025-01-16 15:29:13 +0800

    fix(meta-ui-web): 修复字段消息组件的错误处理和数据绑定
    - 更新 YFieldMessage 组件以使用 useField 而不是 useFieldError
    - 优化错误处理逻辑，支持自定义错误设置
    - 修复模型值同步问题，确保错误状态正确更新

--- meta-ui-web/src/field-message/YFieldMessage.vue
+++ meta-ui-web/src/field-message/YFieldMessage.vue
@@ -1,5 +1,5 @@
 <script setup lang="tsx">
-import {useFieldError, useSetFieldError} from 'vee-validate'
+import {useField, useSetFieldError} from 'vee-validate'
 import type {YFieldMessageEmits, YFieldMessageProps} from './index'
 import {maybeArray} from '@compose/api-model'
 
@@ -8,19 +8,33 @@ const emits = defineEmits<YFieldMessageEmits>()
 
 const _name = toRef(props.name)
 const active = computed(() => !!_name.value)
-const errors = useFieldError(_name)
+const parentForm = inject(YFormInjectionKey)
+const field = useField(_name, void 0, {form: parentForm?.getForm()})
 const setErrorHandler = useSetFieldError(_name)
+
+const setModelValueLock = ref(true)
 const _modelValue = computed<string[]>({
-  get: () => [errors.value],
+  get: () => field.errors.value,
   set: (v: string | string[] | undefined) => {
-    if (!v) return
+    setModelValueLock.value = true
+    if (!v) {
+      setErrorHandler(void 0)
+      return
+    }
     setErrorHandler(v)
     emits('change', maybeArray(v))
     emits('update:modelValue', maybeArray(v))
+    setModelValueLock.value = false
   }
 })
-watch(errors, v => {
-  if (!v) return
+
+watch(field.errors, v => {
+  if (setModelValueLock.value) return
+  console.log('watch error', v)
+  if (!v) {
+    _modelValue.value = []
+    return
+  }
   _modelValue.value = [v]
 })
 </script>
