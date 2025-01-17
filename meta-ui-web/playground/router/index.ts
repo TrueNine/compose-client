1c704096b57687c554ef22070ac7dd02b8f9974a
赵日天 <truenine304520@gmail.com>
2025-01-17 13:56:20 +0800

    refactor(router): 优化路由配置格式
    - 将 routes 数组的定义格式进行调整，增加中括号和大括号的换行
    - 优化代码结构，提高可读性

--- meta-ui-web/playground/router/index.ts
+++ meta-ui-web/playground/router/index.ts
@@ -2,10 +2,12 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 const Router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: '/form',
-    component: () => import('../pages/form/FormPage.vue')
-  }]
+  routes: [
+    {
+      path: '/form',
+      component: () => import('../pages/form/FormPage.vue')
+    }
+  ]
 })
 
 export {Router}
