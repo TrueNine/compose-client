418d55cd15b6a55a190393c04857651c2bfc9838
赵日天 <truenine304520@gmail.com>
2025-02-16 22:31:30 +0800

    refactor(api-types): 更新 WGS84 接口中坐标类型
    - 将 double 类型更改为 f64 类型，以提高代码一致性和可维护性

--- api-types/src/map/index.ts
+++ api-types/src/map/index.ts
@@ -1,9 +1,9 @@
-import type {double} from '@/typescripts'
+import type {f64} from '@/typescripts'
 
 /**
  * 地址坐标数据类型
  */
 export interface WGS84 {
-  x?: double
-  y?: double
+  x?: f64
+  y?: f64
 }
