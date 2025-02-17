76178d63b5f0f65ac31787ebff6e815ca2865205
赵日天 <truenine304520@gmail.com>
2025-02-17 20:37:35 +0800

    refactor(dayjs): 重构 DayJs 类
    - 移除 DayJs 类中的静态方法和属性- 将 DEFAULT_TZ、DEFAULT_UTC 和 DEFAULT_ZONE_OFFSET 提取为常量
    - 重新组织代码结构，使功能模块化- 优化 getDefaultParam 函数，使用新的参数合并方式
    - 重构 timestampOf、timeMillis、datetimeMillis、format 等函数- 移除类中未使用的私有方法

--- extensions/src/dayjs/index.ts
+++ extensions/src/dayjs/index.ts
@@ -7,7 +7,7 @@ import pluralGetSet from 'dayjs/plugin/pluralGetSet'
 import isBetween from 'dayjs/plugin/isBetween'
 import {ISO8601Format, ISO8601TimeZone} from '@compose/api-model'
 
-export type DayJSNewInstanceOptions = string | timestamp | Date | dayjs.Dayjs
+export type DayJSNewInstanceOptions = timestamp | Date | dayjs.Dayjs
 
 interface Params {
   date?: DayJSNewInstanceOptions
@@ -16,129 +16,118 @@ interface Params {
   tz?: string
 }
 
-export class DayJs {
-  static DEFAULT_TZ: string
-  static DEFAULT_UTC: boolean
-  static DEFAULT_ZONE_OFFSET: number
-  static {
-    dayjs.extend(utc)
-    dayjs.extend(tz)
-    dayjs.extend(duration)
-    dayjs.extend(pluralGetSet)
-    dayjs.extend(isBetween)
+export const DEFAULT_TZ = dayjs.tz.guess()
+export const DEFAULT_UTC = DEFAULT_TZ === 'UTC'
+export const DEFAULT_ZONE_OFFSET = dayjs().utcOffset() * 60 * 1000
 
-    this.DEFAULT_TZ = dayjs.tz.guess()
-    this.DEFAULT_UTC = this.DEFAULT_TZ === 'UTC'
-    this.DEFAULT_ZONE_OFFSET = dayjs().utcOffset() * 60 * 1000
+dayjs.extend(utc)
+dayjs.extend(tz)
+dayjs.extend(duration)
+dayjs.extend(pluralGetSet)
+dayjs.extend(isBetween)
+
+export const DayJs = dayjs
+
+function getDefaultParam(date: DayJSNewInstanceOptions, p?: Params, format?: string): rq<Params> {
+  return {
+    date: p?.date ?? date,
+    format: p?.format ?? format ?? ISO8601Format.datetime,
+    utc: (p?.utc ?? DEFAULT_UTC) || false,
+    tz: p?.tz ?? (p?.utc ? ISO8601TimeZone.UTC : void 0) ?? DEFAULT_TZ
   }
+}
 
-  private static getDefaultParam(date: DayJSNewInstanceOptions, p?: Params, format?: string): rq<Params> {
-    return {
-      date: p?.date || date,
-      format: p?.format || format || ISO8601Format.datetime,
-      utc: p?.utc || this.DEFAULT_UTC || false,
-      tz: p?.tz || (p?.utc ? ISO8601TimeZone.UTC : void 0) || this.DEFAULT_TZ
-    }
+function timestampOf(date: DayJSNewInstanceOptions, p?: Params) {
+  const {date: _date, format, tz} = getDefaultParam(date, p, ISO8601Format.datetime)
+  return DayJs.tz(_date, format, tz).valueOf()
+}
+
+export function dateMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
+  const _p = getDefaultParam(date, p, ISO8601Format.date)
+  return timestampOf(_p.date, _p)
+}
+
+export function getOffsetMillis(timezone: string = DEFAULT_TZ): timestamp {
+  return DayJs.tz(timezone).utcOffset() * 60 * 1000
+}
+
+export function timeMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
+  const _p = getDefaultParam(date, p, ISO8601Format.time)
+  if (typeof _p.date === 'string') {
+    _p.format = `${ISO8601Format.date}$$$$${_p.format}`
+    _p.date = `1970-01-01$$$$${_p.date}`
+    const offset = getOffsetMillis(_p.tz)
+    return timestampOf(_p.date, _p) + Number(offset) * 2
+  } else {
+    _p.utc = true
+    _p.tz = ISO8601TimeZone.UTC
+    const dg = timestampToTimeTimestamp(_p.date, _p)
+    if (dg === void 0) return NaN
+    else return dg
   }
+}
 
-  private static D = dayjs
+export function datetimeMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
+  const _p = getDefaultParam(date, p, ISO8601Format.datetime)
+  return timestampOf(_p.date, _p)
+}
 
-  public static get dayjs(): typeof dayjs {
-    return this.D
+export function format(date: DayJSNewInstanceOptions, p?: Params): string {
+  const _p = getDefaultParam(date, p, ISO8601Format.datetime)
+  return DayJs(_p.date, {format: _p.format, utc: _p.utc}, true).tz(_p.tz).format(_p.format)
+}
+
+export function formatDatetime(date: DayJSNewInstanceOptions, p?: Params): string {
+  const _p = getDefaultParam(date, p, ISO8601Format.datetime)
+  return format(_p.date, _p)
+}
+
+export function formatDate(date: DayJSNewInstanceOptions, p?: Params): string {
+  const _p = getDefaultParam(date, p, ISO8601Format.date)
+  return format(_p.date, _p)
+}
+
+export function formatTime(date: DayJSNewInstanceOptions, p?: Params): string {
+  const _p = getDefaultParam(date, p, ISO8601Format.time)
+  return format(_p.date, _p)
+}
+
+/**
+ * ## 将时间戳转换为 time 类型的时间戳
+ * @param ts 时间戳
+ * @param p 参数
+ */
+function timestampToTimeTimestamp(ts: timestamp | Date | dayjs.Dayjs, p?: Params): late<timestamp> {
+  const _p = getDefaultParam(ts, p, ISO8601Format.datetime)
+  let dj: dayjs.Dayjs
+  if (typeof ts === 'number' || typeof ts === 'string') {
+    dj = DayJs(ts, {utc: _p.utc}, true).tz(_p.tz)
+  } else if (ts instanceof Date) {
+    dj = DayJs(ts, {utc: _p.utc}, true).tz(_p.tz)
+  } else {
+    dj = ts.tz(_p.tz)
   }
+  const hMs = dj.hour() * 3600000
+  const mMs = dj.minute() * 60000
+  const sMs = dj.second() * 1000
+  return hMs + mMs + sMs + dj.millisecond()
+}
 
-  public static timestampOf(date: DayJSNewInstanceOptions, p?: Params) {
-    const {date: _date, format, tz} = this.getDefaultParam(date, p, ISO8601Format.datetime)
-    return this.dayjs.tz(_date, format, tz).valueOf()
-  }
+export function isToday(to: DayJSNewInstanceOptions, qua = 1, unit: DurationUnitType = 'day') {
+  const now = dayjs()
+  const yesterday = DayJs(now).subtract(dayjs.duration(qua, unit)).startOf(unit)
+  const toDate = dayjs(to).startOf(unit)
+  return toDate.isSame(yesterday)
+}
 
-  static dateMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.date)
-    return this.timestampOf(_p.date, _p)
-  }
-
-  static getOffsetMillis(timezone: string = this.DEFAULT_TZ): timestamp {
-    return this.dayjs().tz(timezone).utcOffset() * 60 * 1000
-  }
-
-  static timeMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.time)
-    if (typeof _p.date === 'string') {
-      _p.format = `${ISO8601Format.date}$$$$${_p.format}`
-      _p.date = `1970-01-01$$$$${_p.date}`
-      const offset = this.getOffsetMillis(_p.tz)
-      return this.timestampOf(_p.date, _p) + offset * 2
-    } else {
-      _p.utc = true
-      _p.tz = ISO8601TimeZone.UTC
-      const dg = this.timestampToTimeTimestamp(_p.date, _p)
-      if (dg === void 0) return NaN
-      else return dg
-    }
-  }
-
-  static datetimeMillis(date: DayJSNewInstanceOptions, p?: Params): timestamp {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.datetime)
-    return this.timestampOf(_p.date, _p)
-  }
-
-  static format(date: DayJSNewInstanceOptions, p?: Params): string {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.datetime)
-    return this.dayjs(_p.date, {format: _p.format, utc: _p.utc}, true).tz(_p.tz).format(_p.format)
-  }
-
-  static formatDatetime(date: DayJSNewInstanceOptions, p?: Params): string {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.datetime)
-    return this.format(_p.date, _p)
-  }
-
-  static formatDate(date: DayJSNewInstanceOptions, p?: Params): string {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.date)
-    return this.format(_p.date, _p)
-  }
-
-  static formatTime(date: DayJSNewInstanceOptions, p?: Params): string {
-    const _p = this.getDefaultParam(date, p, ISO8601Format.time)
-    return this.format(_p.date, _p)
-  }
-
-  /**
-   * ## 将时间戳转换为 time 类型的时间戳
-   * @param ts 时间戳
-   * @param p 参数
-   */
-  private static timestampToTimeTimestamp(ts: timestamp | Date | dayjs.Dayjs, p?: Params): late<timestamp> {
-    const _p = this.getDefaultParam(ts, p, ISO8601Format.datetime)
-    let dj: dayjs.Dayjs
-    if (typeof ts === 'number') {
-      dj = this.dayjs(ts, {utc: _p.utc}, true).tz(_p.tz)
-    } else if (ts instanceof Date) {
-      dj = this.dayjs(ts, {utc: _p.utc}, true).tz(_p.tz)
-    } else {
-      dj = ts.tz(_p.tz)
-    }
-    const hMs = dj.hour() * 3600000
-    const mMs = dj.minute() * 60000
-    const sMs = dj.second() * 1000
-    return hMs + mMs + sMs + dj.millisecond()
-  }
-
-  static isToday(to: DayJSNewInstanceOptions, qua = 1, unit: DurationUnitType = 'day') {
-    const now = dayjs()
-    const yesterday = dayjs(now).subtract(dayjs.duration(qua, unit)).startOf(unit)
-    const toDate = dayjs(to).startOf(unit)
-    return toDate.isSame(yesterday)
-  }
-
-  static formatToday(to: Dayjs) {
-    const now = dayjs()
-    if (this.isToday(to, 0)) return to.format('HH:mm')
-    if (to.isBefore(now)) {
-      if (this.isToday(to)) return '昨天 ' + to.format('HH:mm')
-      else if (this.isToday(to, 0, 'month')) return to.format('MM-DD')
-      else if (this.isToday(to, 0, 'year')) return to.format('MM-DD')
-      else if (to.year() >= 2000) return to.format('YY-MM-DD')
-      else return to.format('YYYY-MM-DD')
-    } else return to.format('YYYY-MM-DD')
-  }
+export function formatToday(to: Dayjs) {
+  const now = DayJs()
+  if (isToday(to, 0)) return to.format('HH:mm')
+  if (to.isBefore(now)) {
+    if (isToday(to)) return '昨天 ' + to.format('HH:mm')
+    else if (isToday(to, 0, 'month')) return to.format('MM-DD')
+    else if (isToday(to, 0, 'year')) return to.format('MM-DD')
+    else if (to.year() >= 2000) return to.format('YY-MM-DD')
+    else return to.format('YYYY-MM-DD')
+  } else return to.format('YYYY-MM-DD')
 }
