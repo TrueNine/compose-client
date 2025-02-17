6136b1b2c1bb62a7a3ed9a1b48e326e77f2fbc61
赵日天 <truenine304520@gmail.com>
2025-02-17 20:34:05 +0800

    refactor(pino): 重构 Pino 扩展
    - 移除了 Pino 类，改为使用函数导出
    - 优化了代码结构，提高了可读性和可维护性
    - 保留了原有的日志格式和功能

--- extensions/src/pino/index.ts
+++ extensions/src/pino/index.ts
@@ -1,35 +1,31 @@
 import pino, {type ChildLoggerOptions} from 'pino'
 import dayjs from 'dayjs'
 
-export class Pino {
-  private static __LOGGER = pino({
-    timestamp: () => {
-      return dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')
-    },
-    formatters: {},
-    browser: {
-      asObject: true,
-      serialize: true,
-      formatters: {
-        level: label => {
-          return {l: label.toUpperCase()}
-        },
-        log: o => {
-          const _$t = o.time
-          const _$l = o.l
-          const _$p = o.p
-          delete o.time
-          delete o.l
-          delete o.p
-          return {_$t, _$l, _$p, ...o}
-        }
+const _logger = pino({
+  timestamp: () => {
+    return dayjs().format('YYYY-MM-DD HH:mm:ss.SSS')
+  },
+  formatters: {},
+  browser: {
+    asObject: true,
+    serialize: true,
+    formatters: {
+      level: label => {
+        return {l: label.toUpperCase()}
+      },
+      log: o => {
+        const _$t = o.time
+        const _$l = o.l
+        const _$p = o.p
+        delete o.time
+        delete o.l
+        delete o.p
+        return {_$t, _$l, _$p, ...o}
       }
     }
-  })
-  static get instance() {
-    return this.__LOGGER
   }
-  static get logger() {
-    return (cls: string, options?: ChildLoggerOptions<string>) => this.__LOGGER.child({p: cls}, options)
-  }
+})
+
+export function logger() {
+  return (cls: string, options?: ChildLoggerOptions<string>) => _logger.child({p: cls}, options)
 }
