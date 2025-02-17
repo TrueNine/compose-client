a20c7465975db3a6157b9bafa067303de7d5d740
赵日天 <truenine304520@gmail.com>
2025-02-17 23:16:58 +0800

    refactor(api-model): 重构 Keys.ts 文件
    - 移除不必要的类型导入，使用全局的 i32 类型
    - 将 Keys 类中的 generateRandomAsciiString 方法改为顶级函数
    - 删除了未使用的 Int 类型引用

--- api-model/src/tools/Keys.ts
+++ api-model/src/tools/Keys.ts
@@ -1,14 +1,11 @@
-import type {Int} from '@compose/api-types'
-
 import {STRS_ASCII} from '@/consts/Strings'
+import type {i32} from '@compose/api-types'
 
-export class Keys {
-  static generateRandomAsciiString(length: Int = 32) {
-    let s = ''
-    if (length <= 0) return s
-    for (let i = 0; i < length; i++) {
-      s += STRS_ASCII[Math.floor(Math.random() * STRS_ASCII.length)]
-    }
-    return s
+export function generateRandomAsciiString(length: i32 = 32) {
+  let s = ''
+  if (length <= 0) return s
+  for (let i = 0; i < length; i++) {
+    s += STRS_ASCII[Math.floor(Math.random() * STRS_ASCII.length)]
   }
+  return s
 }
