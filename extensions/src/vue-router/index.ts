12f674d63ddfb73b223fcf1ff61dfe3f532958bb
赵日天 <truenine304520@gmail.com>
2025-02-26 12:32:30 +0800

    feat(extensions): 添加 vue-router路由菜单生成功能
    - 新增 generateMenu 函数，用于生成路由菜单
    - 支持自定义路由匹配函数和裁剪路径
    - 递归处理路由配置，生成带有层级结构的菜单对象
    - 提供结合 URI 的辅助函数 combineURIs

---
+++ extensions/src/vue-router/index.ts
@@ -0,0 +1,94 @@
+import type { AutoRouterConfig, late, Maybe } from '@compose/api-types'
+import type { RouteRecordRaw } from 'vue-router'
+
+const STR_EMPTY = ''
+const STR_SLASH = '/'
+
+type RouteMatch = RouteRecordRaw & {
+  fullPath: string
+  parentPath: string
+}
+type Raw = AutoRouterConfig & RouteMatch
+type RouteMatchFn = (r: Raw) => boolean
+
+type MenuObject = {
+  uri: string
+  path: string
+  fullPath: string
+  parentPath: string
+  name?: string
+  iconName?: string
+  tags?: Maybe<string>
+  sub?: MenuObject[]
+} & RouteRecordRaw
+
+function combineURIs(uri1: string, uri2: string): string {
+  if (!uri1.startsWith(STR_SLASH))
+    uri1 = STR_SLASH + uri1
+  uri1 = uri1.replace(/\/$/, STR_EMPTY)
+  uri2 = uri2.replace(/^\//, STR_EMPTY)
+  if (!uri1 && !uri2)
+    return STR_EMPTY
+  if (!uri1)
+    return uri2
+  const result = `${uri1}${STR_SLASH}${uri2}`
+  return result.replace(/\/$/, STR_EMPTY)
+}
+
+/**
+ * ## 生成路由菜单
+ *
+ * @param routes 路由配置
+ * @param matchFn 路由匹配函数
+ * @param clipPath 裁剪路径
+ */
+export function generateMenu(routes: RouteRecordRaw[], matchFn: late<RouteMatchFn> = void 0, clipPath: late<string> = void 0): MenuObject[] {
+  return generateMenuInternal(routes, matchFn, clipPath)
+
+  function generateMenuInternal(
+    routes: RouteRecordRaw[],
+    matchFn: late<RouteMatchFn> = void 0,
+    clipPath: late<string> = void 0,
+    parentPath = '',
+    deepLevel = 0,
+  ): MenuObject[] {
+    if (!deepLevel && clipPath)
+      routes = clip(routes, clipPath)
+    return routes
+      .filter((route) => {
+        return matchFn
+          ? matchFn({
+              redirect: route.redirect,
+              ...route,
+              fullPath: combineURIs(parentPath, route.path),
+              parentPath,
+            } as Raw)
+          : true
+      })
+      .map((route) => {
+        const { meta = {} } = route
+        const menuObj: MenuObject = {
+          ...route,
+          fullPath: combineURIs(parentPath, route.path),
+          parentPath,
+          uri: route.path.replace(/^\/+/, STR_EMPTY),
+          name: meta.title as late<string>,
+        }
+        if (route.children) {
+          menuObj.sub = generateMenuInternal(route.children, matchFn, clipPath, combineURIs(parentPath, route.path), deepLevel + 1)
+        }
+        return menuObj
+      })
+
+    function clip(routes: RouteRecordRaw[], clipPath: string): RouteRecordRaw[] {
+      for (const r of routes) {
+        const fullPath = combineURIs(parentPath, r.path)
+        if (fullPath.startsWith(clipPath) || fullPath.startsWith(clipPath + STR_SLASH))
+          return [...(r.children ?? [])]
+        else if (r.children)
+          return clip(r.children, clipPath)
+      }
+      return []
+    }
+  }
+}
