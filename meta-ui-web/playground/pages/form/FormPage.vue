d6be452db73ef415e8f41072d60ef57310e367ed
赵日天 <truenine304520@gmail.com>
2025-02-28 08:18:13 +0800

    refactor(form): 重构表单页面并添加嵌套对象验证
    - 更新 Yup 引入方式，使用 zod 替代
    - 添加嵌套对象验证规则
    - 移除不必要的 v-model 绑定
    - 调整表单字段顺序
    - 优化代码格式和结构

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -1,33 +1,36 @@
 <script setup lang="ts">
-import {array, object, string} from 'yup'
-import type {dynamic} from '@compose/api-types'
+import type { dynamic } from '@compose/api-types'
 
-const schema = object({
-  files: array().of(string())
+import * as z from 'yup'
+
+const schema = z.object({
+  files: z.array(z.string()),
+  a: z.object({
+    b: z.string().required(),
+  }),
 })
-const submit = (f: dynamic) => {
-  console.log('page submit', JSON.stringify(f))
+
+function submit(s: dynamic) {
+  console.log(s)
 }
-const modelData = ref()
-function set0() {
-  modelData.value.files[0] = '233'
+function error(e: dynamic) {
+  console.error(e)
 }
 </script>
 
 <template>
-  {{ modelData }}
-  <YForm v-model="modelData" class="w-50vw p-4" :schema="schema" @submit="submit">
-    <YFieldMessage name="files" />
+  <YForm class="w-50vw p-4" :schema="schema" @error="error" @submit="submit">
     <YField name="files[0]">
-      <VTextField />
-    </YField>
-    <YField name="a.b">
       <VTextField />
     </YField>
     <YField name="files[1]">
       <VTextField />
     </YField>
-    <VBtn @click="set0">set 0</VBtn>
-    <VBtn type="submit" block color="primary">SUBMIT</VBtn>
+    <YField name="a.b">
+      <VTextField />
+    </YField>
+    <VBtn type="submit" block color="primary">
+      SUBMIT
+    </VBtn>
   </YForm>
 </template>
