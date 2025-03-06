28cad8f49fd8279496ad5902a46cffd57b9c9d3e
赵日天 <truenine304520@gmail.com>
2025-03-06 18:45:00 +0800

    style(meta-ui-web): 优化 YIco 组件样式
    - 在 YIco 组件中添加 display: inline-block 样式，确保图标正确显示为行内块元素
    - 保留原有的 class 属性，以维持其他样式效果

--- meta-ui-web/src/ico/YIco.vue
+++ meta-ui-web/src/ico/YIco.vue
@@ -7,7 +7,7 @@ const props = withDefaults(defineProps<YIcoProps>(), {
 </script>
 
 <template>
-  <Component :is="props.tag" class="inline-block text-6">
+  <Component :is="props.tag" style="display: inline-block" class="text-6">
     <slot name="default" />
   </Component>
 </template>
