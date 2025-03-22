2bd14be0d3ac7675a36bddc46e898a6b5f58bb5b
赵日天 <truenine304520@gmail.com>
2025-03-23 02:11:10 +0800

    perf(meta-ui-web): 优化 YField 组件中的 _effectModels计算逻辑
    - 移除了不必要的空对象检查
    - 用 for 循环替换数组的 map 和 reduce 操作，提高了执行效率
    - 使用 Object.assign 优化了对象合并操作

--- meta-ui-web/src/field/YField.vue
+++ meta-ui-web/src/field/YField.vue
@@ -12,22 +12,21 @@ const _names = useVModel(props, 'name', emits, { passive: true })
 
 const _effectModels = computed(() => {
   const mo = _names.value
-  if (!mo) {
-    return {}
-  }
   if (typeof mo === 'string') {
     return { [mo]: 'modelValue' }
   }
   const names = maybeArray(mo)
-  const res = names.map((e, i) => {
-    if (typeof e === 'string')
-      return { [e]: i === 0 ? 'modelValue' : e }
-    else return e
-  })
-
-  return res.reduce<Record<string, string>>((acc, cur) => {
-    return { ...acc, ...cur }
-  }, {})
+  const result: Record<string, string> = {}
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i]
+    if (typeof name === 'string') {
+      result[name] = i === 0 ? 'modelValue' : name
+    }
+    else {
+      Object.assign(result, name)
+    }
+  }
+  return result
 })
 const mounted = ref(false)
 onMounted(() => {
