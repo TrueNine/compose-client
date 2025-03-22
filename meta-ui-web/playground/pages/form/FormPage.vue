a627dc89765f2c1185e18cfbb6e673cd8299e9cf
赵日天 <truenine304520@gmail.com>
2025-03-22 09:25:47 +0800

    refactor(form): 重构表单页面并添加 PeekView 组件
    - 重构 FormPage.vue，简化代码结构
    - 新增 PeekView.vue 组件用于嵌入式字段
    - 更新表单字段，使用嵌套结构和自定义组件

--- meta-ui-web/playground/pages/form/FormPage.vue
+++ meta-ui-web/playground/pages/form/FormPage.vue
@@ -1,48 +1,37 @@
-<script setup lang="ts">
+<script setup lang="tsx">
 import type { dynamic } from '@compose/api-types'
 
 import * as z from 'zod'
-
-interface Props {
-  modelValue?: dynamic
-}
-const props = withDefaults(
-  defineProps<Props>(),
-  {
-    modelValue: void 0,
-  },
-)
-const emit = defineEmits<{
-  (e: 'update:modelValue', v: dynamic): void
-}>()
-const _modelValue = useVModel(props, 'modelValue', emit, { passive: true })
+import PeekView from './PeekView.vue'
 
 const schema = z.object({
   files: z.array(z.string()),
   a: z.object({
-    b: z.string().default('23344'),
+    b: z.string(),
   }),
-}).optional().refine(v => (v?.files.length ?? 0) > 0, '123123123')
+})
 
 function submit(s: dynamic) {
   console.error({ submit: s })
 }
+
 function error(e: dynamic) {
   console.error(e)
 }
 </script>
 
 <template>
-  <YForm v-model="_modelValue" class="w-50vw p-4" :schema="schema" @error="error" @submit="submit">
-    <div>
-      {{ _modelValue }}
-    </div>
-    <YField name="files[0]">
+  <YForm class="w-50vw p-4" :schema="schema" @error="error" @submit="submit">
+    <YField :name="['modelValue', 'other1', 'other2']">
+      <PeekView />
+    </YField>
+    <YField label="files[0]" name="files[0]">
       <VTextField />
     </YField>
     <YField name="files[1]">
       <VTextField />
     </YField>
+
     <YField name="a.b">
       <VTextField />
     </YField>
---
+++ meta-ui-web/playground/pages/form/PeekView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+interface Props {
+  modelValue?: string
+  other1?: string
+  other2?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: '',
+  other1: '',
+  other2: '',
+})
+const emit = defineEmits(['update:modelValue', 'update:other1', 'update:other2'])
+const { modelValue, other1, other2 } = useVModels(
+  props,
+  emit,
+  { passive: true },
+)
+</script>
+
+<template>
+  <VCard>
+    <VCardText>
+      <VTextField v-model="modelValue" />
+      <VTextField v-model="other1" />
+      <VTextField v-model="other2" />
+    </VCardText>
+  </VCard>
+</template>
