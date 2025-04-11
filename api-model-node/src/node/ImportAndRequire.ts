b4af1a7e36d23e639e4889831bde0cd1ae199177
赵日天 <truenine304520@gmail.com>
2025-04-11 21:23:13 +0800

    chore(api-model-node): 删除 api-model-node 目录下的所有文件以简化项目结构

--- api-model-node/src/node/ImportAndRequire.ts
+++
@@ -1,8 +0,0 @@
-import {createRequire as m} from 'node:module'
-
-/**
- * @param url import.meta.url
- */
-export function createRequire(url: string) {
-  return m(url)
-}
--- api-model-node/src/node/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Files'
-export * from './ImportAndRequire'
