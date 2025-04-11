40da0b5588d745dadaaea8dc927863e128ac96a4
赵日天 <truenine304520@gmail.com>
2025-04-12 00:44:12 +0800

    chore(vue): 移除冗余类型定义和组件安装函数，优化代码结构以提升可读性

--- extensions/src/vue/index.ts
+++ extensions/src/vue/index.ts
@@ -1,6 +1,5 @@
 import type { clip, dynamic, Maybe } from '@compose/api-types'
 import type {
-  App,
   ComponentOptionsMixin,
   ComponentPropsOptions,
   ComputedOptions,
@@ -32,13 +31,6 @@ type ResolveProps<PropsOrPropOptions, E extends EmitsOptions> = Readonly<
 > &
 (object extends E ? object : EmitsToProps<E>)
 
-export interface VueComponentInstanceMapping {
-  name?: string
-  __name?: string
-}
-
-export type SFCWithInstall<T = dynamic> = T & VueComponentInstanceMapping & { install: (app: dynamic) => void }
-
 type SlotNode = VNode<RendererNode, RendererElement, Record<string, dynamic>> & { actualName?: string }
 type NotChildrenSlotNode = clip<SlotNode, 'children'>
 
@@ -69,35 +61,6 @@ export type DefineComponentPart<
   _Props = ResolveProps<Props, Emits>,
   _Defaults = ExtractDefaultPropTypes<Props>,
 > = DefineComponent<Props, Expose, _D, _ComputedOptions, _MethodOptions, Mixin, _ExtendsComponentOptionsMixin, Emits, _EE, _PP, _Props, _Defaults, Slots>
-
-const undefinedName = 'NameUndefined'
-
-/**
- * ## 准备一个安装的组件
- *
- * @param component 组件实例
- * @param otherComponent 其他一同注册的组件实例
- * @returns 封装后的组件
- */
-export function componentInstallToPlugin<T, E = dynamic>(component: T, otherComponent?: Record<string, E>): T {
-  let primaryComponent = component as unknown as SFCWithInstall<T>
-  const otherSecondaryComponentInstallers = otherComponent as unknown as Record<string, SFCWithInstall<T>> | undefined
-  if (!primaryComponent.name)
-    primaryComponent = { ...primaryComponent, name: primaryComponent.__name }
-  primaryComponent.install = (app: App) => {
-    const allInstallComponents = [primaryComponent, ...Object.values(otherSecondaryComponentInstallers ?? {})]
-    for (const toInstallComponent of allInstallComponents) {
-      const { name = void 0, __name = void 0 } = toInstallComponent
-      app.component(name ?? __name ?? undefinedName, toInstallComponent)
-    }
-  }
-  if (otherSecondaryComponentInstallers) {
-    for (const [key, comp] of Object.entries(otherSecondaryComponentInstallers)) {
-      ;(primaryComponent as dynamic)[key] = comp
-    }
-  }
-  return primaryComponent as unknown as T
-}
 
 function _findSlotNodesBy(node: Maybe<SlotNode>, compareFn: (node: NotChildrenSlotNode) => boolean = () => true, result: SlotNode[] = []): SlotNode[] {
   const nodes = maybeArray(node)
