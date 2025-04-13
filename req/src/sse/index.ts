0845eac0c9afe65a861f92b463560d4f0544de35
赵日天 <truenine304520@gmail.com>
2025-04-14 02:06:18 +0800

    refactor(req): 优化 SseReceiver 类的 getter 方法和事件处理函数
    - 为 getter 方法添加返回类型注解，提升代码可读性和类型安全
    - 为事件处理函数添加返回类型注解，确保一致性和清晰性

--- req/src/sse/index.ts
+++ req/src/sse/index.ts
@@ -5,27 +5,27 @@ export class SseReceiver {
   private readonly _url: HttpUrl
   private readonly _init?: EventSourceInit
 
-  get initParam() {
+  get initParam(): EventSourceInit | undefined {
     return this._init
   }
 
-  get url() {
+  get url(): HttpUrl {
     return this._url
   }
 
-  get handle() {
+  get handle(): EventSource {
     return this._e
   }
 
-  onOpen(c: (this: EventSource, ev: Event) => dynamic) {
+  onOpen(c: (this: EventSource, ev: Event) => dynamic): void {
     this.handle.onopen = c
   }
 
-  onMessage<T = dynamic>(c: (this: EventSource, ev: MessageEvent<T>) => dynamic) {
+  onMessage<T = dynamic>(c: (this: EventSource, ev: MessageEvent<T>) => dynamic): void {
     this.handle.onmessage = c
   }
 
-  onError(a: (this: EventSource, event: Event) => dynamic) {
+  onError(a: (this: EventSource, event: Event) => dynamic): void {
     this.handle.onerror = a
   }
 
