8cd91939ce8f8e5953449f50e9b76e3a3e39ea8f
赵日天 <truenine304520@gmail.com>
2025-04-17 15:53:43 +0800

    build(workspace): 调整 vitest.workspace.ts 配置文件，重新排列和添加多个 Vite 配置路径
    - 移动 "./meta-ui-web/vitest.config.ts" 到列表中
    - 重新排列 "./configs/eslint9/vite.config.ts"、"./platform-sdk/wxpa/vite.config.ts"、"./configs/vite-fragment/vite.config.ts"、"./platform-sdk/tmap/vite.config.ts" 和 "./ext/chrome/vite.config.ts" 的顺序，确保配置文件的整洁性和可读性

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -2,16 +2,14 @@ import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
   "./req/vitest.config.ts",
-  "./meta-ui-web/vitest.config.ts",
   "./extensions/vitest.config.ts",
+  "./meta-ui-web/vitest.config.ts",
   "./api-types/vite.config.ts",
   "./api-model/vite.config.ts",
-  "./platform-sdk/tmap/vite.config.ts",
-  "./platform-sdk/wxpa/vite.config.ts",
-  "./uni-app/mp-adk/vite.config.ts",
-  "./uni-app/vite-plugin-uni-shit/vite.config.ts",
-  "./ext/chrome/vite.config.ts",
-  "./configs/eslint9/vite.config.ts",
   "./configs/uno/vite.config.ts",
-  "./configs/vite-fragment/vite.config.ts"
+  "./configs/eslint9/vite.config.ts",
+  "./platform-sdk/wxpa/vite.config.ts",
+  "./configs/vite-fragment/vite.config.ts",
+  "./platform-sdk/tmap/vite.config.ts",
+  "./ext/chrome/vite.config.ts"
 ])
