948c748f8e675685f253b4c6fb9349693c9d6227
赵日天 <truenine304520@gmail.com>
2025-04-19 23:35:44 +0800

    feat: 删除不再使用的扩展相关文件，简化项目结构

--- extensions/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import eslint9 from '@compose/eslint9-config'
-
-export default eslint9({
-  type: 'lib',
-  typescript: {
-    strictTypescriptEslint: true,
-    tsconfigPath: './tsconfig.extensions.json',
-  },
-})
