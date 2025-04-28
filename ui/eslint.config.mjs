2beffba680848aad37a93d2333ff0067c5d5d694
赵日天 <truenine304520@gmail.com>
2025-04-29 04:45:15 +0800

    🔧 [eslint] 精简 ESLint 配置，移除不必要的规则覆盖
    - 🔧 删除 JavaScript 和 TypeScript 中的多个规则覆盖设置，简化配置文件

--- ui/eslint.config.mjs
+++ ui/eslint.config.mjs
@@ -5,22 +5,8 @@ export default eslint9({
   vue: true,
   jsx: true,
   formatters: true,
-  javascript: {
-    overrides: {
-      'no-console': 'off',
-    },
-  },
   typescript: {
     strictTypescriptEslint: true,
     tsconfigPath: './tsconfig.json',
-    overrides: {
-      'ts/no-unsafe-assignment': 'off',
-      'ts/no-unsafe-call': 'off',
-      'ts/no-unsafe-member-access': 'off',
-      'ts/no-unsafe-argument': 'off',
-      'ts/no-unsafe-return': 'off',
-      'ts/no-unsafe-function-type': 'off',
-      'ts/no-unsafe-return': 'off'
-    },
   },
 })
