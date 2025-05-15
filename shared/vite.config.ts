47c9dc1b221ba8b8526e7df718e847ada626b397
赵日天 <truenine304520@gmail.com>
2025-05-16 06:04:47 +0800

    ✨ [shared] 优化 shared 包配置
    - 📦 添加 .npmignore 文件
    - 📝 更新 package.json 中的描述为"共享工具包"
    - 🔧 更新 package.json，调整版本号、files、peerDependencies 和 devDependencies
    - 🔧 更新 vite.config.ts，调整导入路径

--- shared/vite.config.ts
+++ shared/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-import { configureViteFragment } from '@compose/config-vite-fragment'
+import { configureViteFragment } from '@compose/config-vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig(
