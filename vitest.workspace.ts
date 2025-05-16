bb313e420db31cfd28fc328364151fc62d5cbb81
赵日天 <truenine304520@gmail.com>
2025-05-17 07:40:35 +0800

    🔧 [workspace] 添加 design 配置文件到工作区

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,6 +1,7 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
+  './design/vitest.config.ts',
   './req/vitest.config.ts',
   './external/vitest.config.ts',
   './ui/vitest.config.ts',
