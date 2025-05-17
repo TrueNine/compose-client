f7cfad198b7ff7a23fb455d3f725be55a103cdce
赵日天 <truenine304520@gmail.com>
2025-05-18 01:51:39 +0800

    🚚 [design] 组件重命名与结构调整
    - 🚚 将 YdCase.vue 重命名并移动为 device-face/YdDeviceFace.vue
    - 🚚 将 case/index.scss 重命名并移动为 device-face/index.scss
    - 🆕 新增 device-face/index.ts
    - 🆕 新增 src/index.scss
    - 💄 更新 package.json
    - 💄 更新 src/index.ts

---
+++ design/src/components/device-face/index.ts
@@ -0,0 +1,4 @@
+import { componentInstallToPlugin } from '@compose/vue/Install'
+import YdDeviceFace from './YdDeviceFace.vue'
+
+export default componentInstallToPlugin(YdDeviceFace)
--- design/src/index.ts
+++ design/src/index.ts
@@ -1,1 +1,21 @@
-export {}
+import type { App, Plugin } from 'vue'
+import YdDeviceFace from './components/device-face'
+
+export * from './components/device-face'
+
+const components = {
+  YdDeviceFace,
+} as Record<string, unknown>
+
+export {
+  components,
+  YdDeviceFace,
+}
+
+export default {
+  install: (app: App) => {
+    Object.entries(components).forEach(([_name, component]) => {
+      app.use(component as Plugin<[]>)
+    })
+  },
+}
