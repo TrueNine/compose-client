d1168e6ed893474b72e9bfc124212b46192ee7e3
赵日天 <truenine304520@gmail.com>
2025-05-17 12:42:56 +0800

    ⬆️ [version] 更新 eslint9-config 版本至 1.0.11
    - 🧪 禁用 test ts/unbound-method 规则

--- configs/eslint9/src/defaults/index.ts
+++ configs/eslint9/src/defaults/index.ts
@@ -178,6 +178,7 @@ export const defaultStylisticConfig: AntFuStylisticConfig = {
 export const defaultTestConfig: AntFuTestConfig = {
   overrides: {
     'no-console': 'off',
+    'ts/unbound-method': 'off',
     'ts/no-unsafe-argument': 'off',
     'ts/no-unsafe-assignment': 'off',
     'ts/no-unsafe-member-access': 'off',
