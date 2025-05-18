a243b6a1cef099d8aec9ae72f12f40985726f209
赵日天 <truenine304520@gmail.com>
2025-05-18 10:19:24 +0800

    ✨ [config] 更新 vitest.workspace.ts 配置
    - 🆕 新增多个 vite.config.ts 文件路径以支持不同配置
    - 🔄 调整文件顺序以优化工作区结构

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,9 +1,17 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
-  './design/vitest.config.ts',
-  './req/vitest.config.ts',
-  './external/vitest.config.ts',
   './ui/vitest.config.ts',
+  './vue/vitest.config.ts',
+  './req/vitest.config.ts',
+  './types/vite.config.ts',
   './shared/vitest.config.ts',
+  './external/vitest.config.ts',
+  './design/vitest.config.ts',
+  './configs/vite-fragment/vite.config.ts',
+  './configs/eslint9/vite.config.ts',
+  './configs/uno/vite.config.ts',
+  './ext/chrome/vite.config.ts',
+  './psdk/wxpa/vite.config.ts',
+  './psdk/tmap/vite.config.ts',
 ])
