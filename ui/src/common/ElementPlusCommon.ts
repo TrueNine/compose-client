84a6fe126fda6a1b31d0fd67bb682a3384a301eb
赵日天 <truenine304520@gmail.com>
2025-06-22 19:53:15 +0800

    🐛 [eslint] 废弃的规则

--- ui/src/common/ElementPlusCommon.ts
+++ ui/src/common/ElementPlusCommon.ts
@@ -1,9 +1,7 @@
 import type { dynamic } from '@compose/types'
 
 import { dayjs } from 'element-plus'
-// eslint-disable-next-line import/extensions
 import elementEnUs from 'element-plus/dist/locale/en.mjs'
-// eslint-disable-next-line import/extensions
 import elementZhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const zhCn: dynamic = elementZhCn
