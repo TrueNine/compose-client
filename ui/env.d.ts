818004b0cd39d711324b5f2a8d81dfa3e4aa6525
赵日天 <truenine304520@gmail.com>
2025-08-22 08:33:03 +0800

    build: update deps

--- ui/env.d.ts
+++ ui/env.d.ts
@@ -2,3 +2,10 @@
 /// <reference types="vue/jsx" />
 /// <reference types="vitest" />
 /// <reference types="unplugin-vue-router/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
