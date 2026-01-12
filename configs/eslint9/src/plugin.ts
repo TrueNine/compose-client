af340f4c92df1e83c23e7c32c9c258711446b2ba
TrueNine <truenine304520@gmail.com>
2026-01-12 14:13:59 +0800

    style(eslint9): simplify multi-line objects to single line
    - Consolidate plugin object definition from multiple lines to single line
    - Combine meta object properties (name and version) on one line
    - Improve code readability by reducing unnecessary line breaks

--- configs/eslint9/src/plugin.ts
+++ configs/eslint9/src/plugin.ts
@@ -1,8 +1,6 @@
 import type {ESLint} from 'eslint'
 import {rules} from './rules'
 
-export const plugin: ESLint.Plugin = {meta: {name: '@truenine/eslint-plugin',
-  version: '1.0.0'},
-rules}
+export const plugin: ESLint.Plugin = {meta: {name: '@truenine/eslint-plugin', version: '1.0.0'}, rules}
 
 export default plugin
