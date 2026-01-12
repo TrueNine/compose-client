66736b1adf1e219c1623ad26c463721287db7522
TrueNine <truenine304520@gmail.com>
2026-01-12 14:14:22 +0800

    style(eslint9): simplify multi-line type exports to single line
    - Consolidate multi-line type export statement into single line
    - Improve code readability by reducing vertical spacing
    - Maintain all exported types: AntFuConfig, AntFuFormatterConfig, AntFuJsConfig, AntFuStrictTsConfig, AntFuStylisticConfig, AntFuTestConfig, AntFuTsConfig, AntFuUnocssConfig, AntFuVueConfig

--- configs/eslint9/src/types/index.ts
+++ configs/eslint9/src/types/index.ts
@@ -4,15 +4,7 @@
  */
 
 // AntFu 相关类型
-export type {AntFuConfig,
-  AntFuFormatterConfig,
-  AntFuJsConfig,
-  AntFuStrictTsConfig,
-  AntFuStylisticConfig,
-  AntFuTestConfig,
-  AntFuTsConfig,
-  AntFuUnocssConfig,
-  AntFuVueConfig} from './antfu'
+export type {AntFuConfig, AntFuFormatterConfig, AntFuJsConfig, AntFuStrictTsConfig, AntFuStylisticConfig, AntFuTestConfig, AntFuTsConfig, AntFuUnocssConfig, AntFuVueConfig} from './antfu'
 
 // 配置选项类型
 export type {ConfigOptions} from './options'
